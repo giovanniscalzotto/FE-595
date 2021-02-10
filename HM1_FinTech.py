38192aaba6b0f7ff92fb3f8779344a7c81bfff81
nothiefhouse <78397321+nothiefhouse@users.noreply.github.com>
2021-02-10 16:53:23 -0500

    Create HM1_FinTech.py
    FForner develop

---
+++ HM1_FinTech.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+time = np.arange( 0, 2*np.pi, 0.01 )
+sine = np.sin( time )
+cosine = np.cos( time )
+
+
+plt.plot( time, sine, label= "Sine" )
+plt.plot( time, cosine, label= "Cosine" )
+
+
+plt.title( 'One Period Sine and Cosine' )
+plt.xlabel( 'Frequncy' )
+plt.ylabel( 'Amplitude' )
+
+
+plt.axhline( y = 0, color='k' )
+plt.axvline( x = 0, color='k', linestyle='-')
+plt.legend(loc="upper center")
+
+
+plt.show( )
